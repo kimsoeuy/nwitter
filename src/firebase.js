34085c341fb3aee8775f44df46dbbbaad33011e6
kimsoeuy <soeuy5309@naver.com>
2021-08-28 00:23:28 +0900

    1.0 Initialization

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import * as firebase from 'firebase/app'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCJ4Ulg7aqZG2lGxP0Kpjm7r1c4th16G_U",
+    authDomain: "nwitter-e169b.firebaseapp.com",
+    projectId: "nwitter-e169b",
+    storageBucket: "nwitter-e169b.appspot.com",
+    messagingSenderId: "558999161994",
+    appId: "1:558999161994:web:36c4edad0c4ad31c377173",
+    measurementId: "G-W4VB0K4NX7"
+  };
+
+  export default firebase.initializeApp(firebaseConfig);
